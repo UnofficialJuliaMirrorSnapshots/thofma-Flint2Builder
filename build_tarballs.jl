601f12debf2d41a836f01507d160c62e7d7d3152
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 05:14:15 +0000

    Snapshot of branch master taken on 2019-04-08T01:14:14.655-04:00 from "https://github.com/UnofficialJuliaMirror/thofma-Flint2Builder"

---
+++ build_tarballs.jl
@@ -0,0 +1,46 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "libflint"
+version = v"0.0.0-8b392f5056b0d4626e7d9ddc472c835ba82926db"
+
+# Collection of sources required to build libflint
+sources = [
+    "https://github.com/wbhart/flint2.git" =>
+    "8b392f5056b0d4626e7d9ddc472c835ba82926db",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd flint2/
+./configure --prefix=$prefix --disable-static --enable-shared --with-gmp=$prefix --with-mpfr=$prefix
+make -j${nproc}
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Windows(:x86_64),
+    MacOS(:x86_64),
+    Linux(:x86_64, libc=:glibc)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libflint", Symbol("libflint insta\x01\e[A\x01\e[B\e[B"))
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "https://github.com/JuliaMath/GMPBuilder/releases/download/v6.1.2-2/build_GMP.v6.1.2.jl",
+    "https://github.com/JuliaMath/MPFRBuilder/releases/download/v4.0.1-3/build_MPFR.v4.0.1.jl"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
