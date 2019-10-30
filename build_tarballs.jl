80c95184632b7763047d9dac3c8de0b5519ed263
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-30 18:43:18 +0000

    Snapshot of branch master taken on 2019-10-30T14:43:17.748-04:00 from "https://github.com/UnofficialJuliaMirror/thofma-Flint2Builder"

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder
 
 name = "libflint"
-version = v"0.0.0-c5852387025bf144f32c0593f0ecc906c81266f1"
+version = v"0.0.0-4c966126d6390f0479dbd570afd434498af86a7a"
 
 # Collection of sources required to build libflint
 sources = [
     "https://github.com/wbhart/flint2.git" =>
-    "c5852387025bf144f32c0593f0ecc906c81266f1",
+    "4c966126d6390f0479dbd570afd434498af86a7a",
 
 ]
 
